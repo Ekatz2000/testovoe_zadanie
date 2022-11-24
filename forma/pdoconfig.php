202ef4b91c13059bcfb03bfb211c24e91774db0e
EkaterinaZubenko <99827290+Ekatz2000@users.noreply.github.com>
2022-11-24 22:49:18 +0300

    Delete forma directory

--- forma/pdoconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $db_base = 't953727y_ek'; // Имя БД
-    $db_user = "t953727y_ek"; // Логин БД
-    $db_password = "111111EKz"; // Пароль БД
-	$db_host = "localhost"; 
-    $db_table = "dannye"; // Имя Таблицы БД
-?>
